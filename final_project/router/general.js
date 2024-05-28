ec6c78df1c9c7786cd6a21e57644e3361d797ca6
ADO-bit <adnandruzic@gmail.com>
2024-05-28 07:13:23 -0400

    Tested GET book reviews by ISBN

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -35,18 +35,19 @@ public_users.get('/isbn/:isbn',function (req, res) {
   });
   
 // Get book details based on author
-public_users.get('/author/:author',function (req, res) {
-    const isbn = req.params.isbn;
-    const book = books[isbn];
-    if (book) {
-      return res.status(200).json(book);
+public_users.get('/author/:author', (req, res) => {
+    const author = req.params.author;
+    const result = Object.values(books).filter(book => book.author === author);
+    if (result.length > 0) {
+      return res.status(200).json(result);
     } else {
-      return res.status(404).json({ message: "Book not found" });
+      return res.status(404).json({ message: "Books not found" });
     }
   });
+  
 
 // Get all books based on title
-public_users.get('/title/:title',function (req, res) {
+public_users.get('/title/:title', (req, res) => {
     const title = req.params.title;
     const result = Object.values(books).filter(book => book.title === title);
     if (result.length > 0) {
